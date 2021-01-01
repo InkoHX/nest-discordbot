78629b8833e357f0ccd48f817b4484f1b33824c0
InkoHX <me@inkohx.dev>
2021-01-01 20:48:46 +0900

    minor update
    - remove getClient
    - add getReadyTimestamp
    - add getUptime

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,10 +4,19 @@ import { BotService } from './bot/bot.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: BotService) {}
+  constructor(private readonly botService: BotService) {}
 
-  @Get()
-  getClient() {
-    return this.appService
+  @Get('readyTimestamp')
+  getReadyTimestamp() {
+    return {
+      timestamp: this.botService.readyTimestamp,
+    }
+  }
+
+  @Get('uptime')
+  getUptime() {
+    return {
+      uptime: this.botService.uptime,
+    }
   }
 }
